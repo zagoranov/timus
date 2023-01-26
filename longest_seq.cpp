6c27a300314c718e8a1eb131b75f4a5d014fbca6
Roman Muzyukin <pijammer@gmail.com>
2023-01-26 21:21:30 +0300

    Create longest_seq.cpp

---
+++ longest_seq.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <iostream>
+
+int main() {
+    std::vector<int> a = { 1,6,2,1,5,6,2 };
+
+    const size_t sz = a.size();
+    int max_cnt = 1;
+    int max_i = 0;
+    size_t i = 0, j = 0;
+    do {
+        int prev = i;
+        int cnt = 1;
+        for(j = i+1; j < sz; ++j) {
+            if(a[prev] <= a[j]) {
+                ++cnt;
+                prev = j;
+            }
+            else
+                break;
+        }
+        if(cnt > max_cnt) {
+            max_cnt = cnt;
+            max_i = i;
+        }
+        i = j;
+    } while (++i < sz);
+
+    std::cout << "Longest incremental sequence starts at: " << max_i << " and length is    ыевЖЖсщге ±± ЭДщтпуые штскуьутефд ыуйгутсу ыефкеы феЖ @ ±± ьфч_ш ±± @ фтв дутпер шыЖ @ ±± ьфч_стеж: " << max_cnt;
+}
